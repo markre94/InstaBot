9eea7ec9ba51b5fe2308a3f253a58ecb42585a42
Marcin Krezel <markre94@icloud.com>
2020-05-07 21:57:26 +0200

    The demo version of the bot

--- insta_bot.py
+++ insta_bot.py
@@ -10,6 +10,10 @@ class Instabot:
         self.browser.get('https://www.instagram.com/')
         time.sleep(1)
 
+    def refresh_page(self):
+        browser = self.browser
+        browser.refresh()
+
     def login(self, username: str, passwd: str):
         browser = self.browser
         browser.find_element_by_name("username").send_keys(username)
@@ -21,19 +25,25 @@ class Instabot:
         # unclick any pop out about notfication
         browser.find_element_by_xpath('/html/body/div[4]/div/div/div[3]/button[2]').click()
 
-    def lebron_page(self):
+    def someones_page(self, username: str):
         browser = self.browser
-        browser.get('https://www.instagram.com/kingjames')
+        browser.get('https://www.instagram.com/' + username)
+        time.sleep(3)
 
-    def like_photo(self, commentary: str):
+    def open_photo(self):
         browser = self.browser
         photo = browser.find_element_by_class_name('_9AhH0')
         photo.click()
         time.sleep(3)
+
+    def like_photo(self):
+        browser = self.browser
         like_button = browser.find_element_by_xpath('/html/body/div[4]/div[2]/div/article/div[2]/section[1]/span[1]')
         like_button.click()
         time.sleep(1)
 
+    def post_comment(self, commentary):
+        browser = self.browser
         # CLick on a text bar
         comment_space = browser.find_element_by_class_name('Ypffh')
         comment_space.click()
@@ -43,10 +53,37 @@ class Instabot:
         browser.find_element_by_xpath('/html/body/div[4]/div[2]/div/article/div[2]/section[3]/div/form/button').click()
         pass
 
+    def count_posts(self):
+        browser = self.browser
+        post_count = browser.find_element_by_class_name('g47SY').text
+        return int(post_count.replace(' ', ''))
+        pass
+
 
 if __name__ == '__main__':
     bot1 = Instabot()
     bot1.login('krezel94', my_secret_password)
-    bot1.lebron_page()
-    bot1.like_photo(commentary='Great post!')
+    bot1.someones_page('krezel94')
+    current_number = bot1.count_posts()
+    time.sleep(3)
+
+    # Check for the new added photos
+
+    while True:
+        time.sleep(3)
+        new_number = bot1.count_posts()
+        if new_number == current_number:
+            time.sleep(5)
+            bot1.refresh_page()
+        else:
+            time.sleep(1)
+            bot1.open_photo()
+            bot1.like_photo()
+            bot1.post_comment('<3')
+            time.sleep(1)
+            break
+
+    # bot1.open_photo()
+    # bot1.like_photo()
+    # bot1.post_comment('Keep up a good work!')
     pass
